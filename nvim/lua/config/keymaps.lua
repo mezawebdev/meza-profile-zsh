2d5cf95b1799e805bb45406d1382e711d6acaf2c
Alex Meza <mezawebdev@gmail.com>
2026-01-10 11:46:43 -0800

    fix: Mouse after Wave terminal switch

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Fix mouse after Wave terminal workspace switch (Wave doesn't send focus events)
+vim.keymap.set("n", "<leader>m", function()
+  vim.o.mouse = ""
+  vim.cmd("redraw")
+  vim.o.mouse = "a"
+  vim.notify("Mouse reset")
+end, { desc = "Reset mouse mode" })
