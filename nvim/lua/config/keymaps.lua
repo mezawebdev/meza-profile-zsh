636443f8b3a239052afe49fcccbb80bfb179937c
Alex Meza <mezawebdev@gmail.com>
2026-01-11 21:45:21 -0800

    updates

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,10 +2,23 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Toggle terminal with Ctrl+j
-vim.keymap.set({ "n", "t" }, "<C-j>", function()
+-- Toggle terminal with Ctrl+\
+vim.keymap.set({ "n", "t" }, "<C-\\>", function()
   require("snacks").terminal()
 end, { desc = "Toggle Terminal" })
+
+-- Copy file paths to clipboard
+vim.api.nvim_create_user_command("CopyRelFilePath", function()
+  local path = vim.fn.expand("%")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied: " .. path)
+end, {})
+
+vim.api.nvim_create_user_command("CopyAbsFilePath", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied: " .. path)
+end, {})
 
 -- Fix mouse after Wave terminal workspace switch (Wave doesn't send focus events)
 vim.keymap.set("n", "<leader>m", function()
