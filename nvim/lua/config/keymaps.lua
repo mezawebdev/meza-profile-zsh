2aeb722467eca83a1c1f514b09c0fb5ab90004c7
Alex Meza <mezawebdev@gmail.com>
2026-03-02 23:02:31 -0800

    feat: Add macOS-style Nvim keymaps and Prominent Focus Warp theme

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -27,3 +27,16 @@ vim.keymap.set("n", "<leader>m", function()
   vim.o.mouse = "a"
   vim.notify("Mouse reset")
 end, { desc = "Reset mouse mode" })
+
+-- Insert Mode: Cmd + Arrows (macOS style navigation)
+vim.keymap.set("i", "<D-Left>", "<Home>", { desc = "Move to beginning of line" })
+vim.keymap.set("i", "<D-Right>", "<End>", { desc = "Move to end of line" })
+vim.keymap.set("i", "<D-Up>", "<C-o>gg", { desc = "Move to top of file" })
+vim.keymap.set("i", "<D-Down>", "<C-o>G", { desc = "Move to bottom of file" })
+
+-- Bonus: Option + Arrows for jumping by word
+vim.keymap.set("i", "<M-Left>", "<C-o>b", { desc = "Jump word left" })
+vim.keymap.set("i", "<M-Right>", "<C-o>w", { desc = "Jump word right" })
+-- Insert mode: Option + Up/Down to move the current line up or down
+vim.keymap.set("i", "<M-Up>", "<Esc>:m .-2<CR>==gi", { desc = "Move line up" })
+vim.keymap.set("i", "<M-Down>", "<Esc>:m .+1<CR>==gi", { desc = "Move line down" })
