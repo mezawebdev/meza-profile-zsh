4f66af380b2aec99d942bfaa12536ab2ad4ac6f6
Alex Meza <mezawebdev@gmail.com>
2026-03-17 23:39:37 -0700

    force lazygit to start in halfmode

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -9,6 +9,9 @@ return {
           height = 0,
         },
         config = {
+          gui = {
+            screenMode = "half",
+          },
           os = {
             edit = '[ -z "$NVIM" ] && (nvim -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{filename}})',
             editAtLine = '[ -z "$NVIM" ] && (nvim +{{line}} -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{filename}} && nvim --server "$NVIM" --remote-send ":{{line}}<CR>")',
