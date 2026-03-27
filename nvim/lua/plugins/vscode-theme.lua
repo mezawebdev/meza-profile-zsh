ef1ae39f1cb5bb1c6e75527f54da8b8f2998db7d
Alex Meza <alexmeza@Alexs-MacBook-Air.local>
2026-03-27 12:40:22 -0700

    update

--- nvim/lua/plugins/vscode-theme.lua
+++ nvim/lua/plugins/vscode-theme.lua
@@ -10,7 +10,21 @@ return {
     },
   },
 
-  -- 2. Tell LazyVim to use it as the default colorscheme
+  -- 2. Make the neo-tree cursor line visible even when unfocused
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    init = function()
+      vim.api.nvim_set_hl(0, "NeoTreeCursorLine", { bg = "#333842", bold = true })
+      vim.api.nvim_create_autocmd("WinLeave", {
+        pattern = "neo-tree *",
+        callback = function()
+          vim.wo.cursorline = true
+        end,
+      })
+    end,
+  },
+
+  -- 3. Tell LazyVim to use it as the default colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
