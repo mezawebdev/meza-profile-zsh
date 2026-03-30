101f852d0ea762fc9ba172a77bd2720d4a1b21dd
Alex Meza <alexmeza@Alexs-MacBook-Air.local>
2026-03-30 10:40:38 -0700

    introduce claude code plugin

---
+++ nvim/lua/plugins/claudecode.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "coder/claudecode.nvim",
+    config = true,
+    opts = {
+      -- Don't open Claude in a Neovim terminal - we run it in cmux
+      terminal = {
+        provider = "none",
+      },
+    },
+    keys = {
+      { "<leader>as", "<cmd>ClaudeCodeSend<cr>", desc = "Send to Claude", mode = "v" },
+      { "<leader>aa", "<cmd>ClaudeCodeAdd<cr>", desc = "Add file to Claude" },
+    },
+  },
+}
